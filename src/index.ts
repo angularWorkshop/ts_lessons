1c3346930029fb7da48e57c43ae375a9646fe72b
Max <max@MacBook-Pro-Max.local>
2026-04-05 21:51:10 +0300

    Add Topic 16 Exercise 2 answer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export const routes = ['/', '/courses', '/profile'];
+export const routes = ['/', '/courses', '/profile'] as const;
 
-export type RoutePath = string;
+export type RoutePath = (typeof routes)[number];
 
 export function navigateTo(route: RoutePath): string {
   return `Navigating to ${route}`;
