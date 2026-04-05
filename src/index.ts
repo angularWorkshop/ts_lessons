561fb9e739e8d154727982f6aead8d712dbbb380
Max <max@MacBook-Pro-Max.local>
2026-04-05 20:18:05 +0300

    Add topic 06 exercise 01 solution

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+export function add(first: number, second: number): number;
+export function add(first: string, second: string): string;
 export function add(first: string | number, second: string | number): string | number {
+  if (typeof first === 'number' && typeof second === 'number') {
+    return first + second;
+  }
+
   return `${first}${second}`;
 }
 
-export function sumAll(...values: (string | number)[]): number {
-  return values.length;
+export function sumAll(...values: number[]): number {
+  return values.reduce((total: number, value: number) => total + value, 0);
 }
