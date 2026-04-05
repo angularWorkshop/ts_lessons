832b80676e47a3d845691c3990cc6332a0235bf9
Max <max@MacBook-Pro-Max.local>
2026-04-05 18:31:43 +0300

    Add topic 05 exercise 01 solution

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 export interface User {
   id: number;
   name: string;
+  email?: string;
 }
 
-export interface Admin {
-  permissions: string[];
+export interface Admin extends User {
+  readonly permissions: readonly string[];
 }
 
 export const adminUser: Admin = {
+  id: 100,
+  name: 'Course Admin',
   permissions: ['manage-users', 'publish-course'],
 };
 
 export function printUser(user: User): string {
-  return user.name;
+  const email = user.email ?? 'no email';
+
+  return `${user.name} <${email}>`;
 }
