606782996f13546a792a7467a8a20e9f48138ee1
Max <max@MacBook-Pro-Max.local>
2026-04-05 20:56:04 +0300

    Add Topic 11 Exercise 1 answer

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export type UnwrapPromise<T> = T;
+export type UnwrapPromise<T> = T extends Promise<infer Value> ? UnwrapPromise<Value> : T;
 
-export async function resolveValue<T>(value: T): Promise<T> {
-  return value;
+export async function resolveValue<T>(value: T): Promise<UnwrapPromise<T>> {
+  return (await value) as UnwrapPromise<T>;
 }
