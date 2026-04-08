e9a9fcd72f28ad31f2451a4f9fb460a6291a43f0
Max <max@MacBook-Pro-Max.local>
2026-04-08 20:05:29 +0300

    Clean Topic 28 exercise 01 build artifacts

--- packages/core/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { type DashboardCard, type MetricSnapshot } from '@app/shared';
-export interface DashboardInput extends MetricSnapshot {
-    delta: number;
-}
-export declare function buildDashboardCards(metrics: DashboardInput[]): DashboardCard[];
-//# sourceMappingURL=index.d.ts.map
